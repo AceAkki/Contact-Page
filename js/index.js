e1ab0fce76fc53f05e5ff39786bd4c969ce1953f
Akshay P (A-K-SHA-Y) <140274081+AceAkki@users.noreply.github.com>
2025-08-28 00:17:49 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,40 +1,15 @@
 
 
-let m1024 = window.matchMedia("(min-width:1024px)");
-let m360 = window.matchMedia("(min-width: 360px)");
 
-class Form {
-
-  formDropDown (btnSelector, listSelector) {
-    document.querySelector(btnSelector).addEventListener("click", () => {
-      document.querySelector(listSelector).classList.toggle("hide");
-    });
-  }
-
-  formChecked(selector) {
-     document.querySelector(selector).addEventListener("click", (e) => {
-      if (e.target.closest(".form-item")) {
-        if (!e.target.closest(".form-item").querySelector("img")) {
-          let createImg = document.createElement("img");
-          createImg.src = "img/checkMark.svg";
-          e.target.closest(".form-item").appendChild(createImg);
-        }
-      }
-    });
-  }
-
-}
-const classForm = new Form();
-
-document.addEventListener("DOMContentLoaded", documentLoaded());
-
-
-function documentLoaded() {
+(function init(){
+  document.addEventListener("DOMContentLoaded", () => {
     let header = document.querySelector(".header-wrap");
     let headerButton = document.querySelector(".header-btn");
     let headerLinks = document.querySelector(".header-links");
     let form = document.querySelector(".enquiry-form");
-
+    
+    let m1024 = window.matchMedia("(min-width:1024px)");
+    let m360 = window.matchMedia("(min-width: 360px)");
     getHeaderHeight(header);
     scrollHeader(header);
 
@@ -48,18 +23,81 @@ function documentLoaded() {
     }
 
 
-    classForm.formDropDown("#queryBtn","#queryType");
-    classForm.formChecked("#queryType");
+    classForm.formDropDown("#queryBtn","#queryType", "hide");
+    classForm.formChecked("#queryType", ".form-item", "checked", "hide");
 
 
     document.querySelector(".button-map").addEventListener("click", ()=> {
       form.classList.remove("hide");
     })
     document.querySelector(".cancel-btn").addEventListener("click", ()=> {
-      form.classList.add("hide");
+      form.classList.add("hide"); 
     })
+});
+})()
+
+class Form {
+  formDropDown (btnSelector, elemSelector, classToToggle) {
+    document.querySelector(btnSelector).addEventListener("click", () => {
+      this.toggleClass (elemSelector, classToToggle)
+    });
+  }
+
+  toggleClass (elemSelector, classToToggle){
+    console.log(elemSelector, classToToggle)
+    document.querySelector(elemSelector).classList.toggle(classToToggle);
+  }
+
+  formChecked(selector, targetClass, checkClass, classToToggle) {
+     document.querySelector(selector).addEventListener("click", (e) => {
+      if (e.target.closest(targetClass) && !(document.getElementsByClassName(checkClass).length > 0)) {
+        let targetItem = e.target.closest(targetClass);
+        let targetValue;
+        if (targetItem.querySelector("img")) return;
+        let createImg = document.createElement("img");
+        createImg.src = "img/checkMark.svg";
+        targetItem.appendChild(createImg);
+        targetItem.classList.add(checkClass);
+        targetValue = targetItem.getAttribute("data-value");    
+
+        if (targetValue) {
+          setTimeout( ()=> {
+            document.querySelectorAll(targetClass).forEach(elm => { elm.classList.remove(checkClass) });
+            targetItem.querySelector("img").remove();
+            this.toggleClass(selector, classToToggle);
+            this.createOptions(document.querySelector(selector).parentNode.parentNode, targetItem.closest(".form-group"), targetValue)
+          }, 2000)
+        }
+
+      }
+    });
+  }
+
+  createOptions(mainParent, parentNode, targetValue){
+    let targetGroup = document.createElement("div");
+    let targetSelect = document.createElement("div");
+    let targetTitle = document.createElement("span");
+    targetGroup.classList.add("form-group");
+    targetSelect.classList.add("form-select");
+    targetTitle.textContent = targetValue;
+    mainParent.appendChild(targetGroup);
+    mainParent.insertBefore(targetGroup, parentNode.nextSibling);
+    targetGroup.appendChild(targetSelect);
+    targetSelect.appendChild(targetTitle);
+
+    let targetOptions = document.createElement("div");
+    targetOptions.classList.add("form-options", "hide");
+    
+
+    
+
+    
+  }
+  
+
 
 }
+const classForm = new Form();
 
 
 function getHeaderHeight(header) {
