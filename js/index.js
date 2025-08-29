84299a5168109c3df18a9b6bb68483f20c9c1411
Akshay P (A-K-SHA-Y) <140274081+AceAkki@users.noreply.github.com>
2025-08-29 23:52:34 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,87 +1,111 @@
-
-
-
-(function init(){
+(function init() {
   document.addEventListener("DOMContentLoaded", () => {
     let header = document.querySelector(".header-wrap");
     let headerButton = document.querySelector(".header-btn");
     let headerLinks = document.querySelector(".header-links");
     let form = document.querySelector(".enquiry-form");
-    
+
     let m1024 = window.matchMedia("(min-width:1024px)");
     let m360 = window.matchMedia("(min-width: 360px)");
     getHeaderHeight(header);
     scrollHeader(header);
 
-    
     if (m1024.matches) {
-      form.classList.add("hide");     
+      form.classList.add("hide");
     } else if (m360.matches) {
-      form.classList.remove("hide");     
-      mobileDropdown(headerButton, headerLinks);   
-
+      form.classList.remove("hide");
+      mobileDropdown(headerButton, headerLinks);
     }
-
 
     // classForm.formDropDown("#queryBtn","#queryType", "hide");
     // classForm.formChecked("#queryType", ".form-item", "checked", "hide");
-    classForm.formDropDown(form.querySelector("form"), ".form-select", ".form-group", ".form-options", "hide");
-    classForm.formChecked(form.querySelector("form"), ".form-options", ".form-item", "checked", "hide");
+    classForm.formDropDown(
+      form.querySelector("form"),
+      ".form-select",
+      ".form-group",
+      ".form-options",
+      "hide"
+    );
+    classForm.formChecked(
+      form.querySelector("form"),
+      ".form-options",
+      ".form-item",
+      "checked",
+      "hide"
+    );
 
-    document.querySelector(".button-map").addEventListener("click", ()=> {
+    document.querySelector(".button-map").addEventListener("click", () => {
       form.classList.remove("hide");
-    })
-    document.querySelector(".cancel-btn").addEventListener("click", ()=> {
-      form.classList.add("hide"); 
-    })
-});
-})()
+    });
+    document.querySelector(".cancel-btn").addEventListener("click", () => {
+      form.classList.add("hide");
+    });
+  });
+})();
 
 class Form {
-  formDropDown (form, btnSelector, parentSelector, elemSelector, classToToggle) {
-    console.log(form)
-    form.addEventListener("click", (e)=> {
-      if(e.target.closest(btnSelector)) {
-        this.toggleClass (e.target.closest(btnSelector).closest(parentSelector).querySelector(elemSelector), classToToggle)
+  formDropDown(form, btnSelector, parentSelector, elemSelector, classToToggle) {
+    form.addEventListener("click", (e) => {
+      if (e.target.closest(btnSelector)) {
+        console.log(
+          e.target
+            .closest(btnSelector)
+            .closest(parentSelector)
+            .querySelector(elemSelector),
+          classToToggle
+        );
+        this.toggleClass(
+          e.target
+            .closest(btnSelector)
+            .closest(parentSelector)
+            .querySelector(elemSelector),
+          classToToggle
+        );
       }
-    }) 
+    });
   }
 
-  toggleClass (elem, classToToggle){
+  toggleClass(elem, classToToggle) {
     elem.classList.toggle(classToToggle);
   }
 
   formChecked(form, selector, targetClass, checkClass, classToToggle) {
-    form.addEventListener("click", (e)=> {
-      if(e.target.closest(selector)) {
+    form.addEventListener("click", (e) => {
+      if (e.target.closest(selector)) {
         if (e.target.closest(targetClass) && !(document.getElementsByClassName(checkClass).length > 0)) {
-        let targetItem = e.target.closest(targetClass);
-        let targetValue;
-        if (targetItem.querySelector("img")) return;
-        let createImg = document.createElement("img");
-        createImg.src = "img/checkMark.svg";
-        targetItem.appendChild(createImg);
-        targetItem.classList.add(checkClass);
-        targetValue = targetItem.getAttribute("data-value");    
-
-        if (targetValue) {
-          setTimeout( ()=> {
-            document.querySelectorAll(targetClass).forEach(elm => { elm.classList.remove(checkClass) });
+          let targetItem = e.target.closest(targetClass);
+          let targetValue;
+          if (targetItem.querySelector("img")) return;
+          let createImg = document.createElement("img");
+          createImg.src = "img/checkMark.svg";
+          targetItem.appendChild(createImg);
+          targetItem.classList.add(checkClass);
+          targetValue = targetItem.getAttribute("data-value");
+          if (!targetValue) return 
+          setTimeout(() => {
+            document.querySelectorAll(targetClass).forEach((elm) => {
+              elm.classList.remove(checkClass);
+            });
             targetItem.querySelector("img").remove();
-            this.toggleClass(document.querySelector(selector), classToToggle);
-            this.createOptions(document.querySelector(selector).parentNode.parentNode, targetItem.closest(".form-group"), targetValue)
-          }, 2000)
+            console.log(document.querySelector(selector));
+            this.toggleClass(targetItem.closest(selector), classToToggle);
+            this.createOptions(
+              document.querySelector(selector).parentNode.parentNode,
+              targetItem.closest(".form-group"),
+              targetValue
+            );
+          }, 2000);
+        
+        
         }
-
       }
-      }
-    })
+    });
     //  document.querySelector().addEventListener("click", (e) => {
-     
+
     // });
   }
 
-  createOptions(mainParent, parentNode, targetValue){
+  createOptions(mainParent, parentNode, targetValue) {
     let targetGroup = document.createElement("div");
     let targetSelect = document.createElement("div");
     let targetTitle = document.createElement("span");
@@ -97,25 +121,20 @@ class Form {
     targetOptions.classList.add("form-options", "hide");
     targetGroup.appendChild(targetOptions);
 
-    Array.from([...Array(8).keys()]).forEach(num => {
+    Array.from([...Array(8).keys()]).forEach((num) => {
       if (num > 0) {
         let elmOption = document.createElement("div");
         elmOption.classList.add("form-item");
-        elmOption.setAttribute("data-value",`${targetValue}-${num}`);
+        elmOption.setAttribute("data-value", `${targetValue}-${num}`);
         let optionSpan = document.createElement("span");
         optionSpan.textContent = `${targetValue}-${num}`;
         targetOptions.appendChild(elmOption);
         elmOption.appendChild(optionSpan);
-
       }
-    })    
+    });
   }
-  
-
-
 }
 const classForm = new Form();
-
 
 function getHeaderHeight(header) {
   let headerHeight = header.getBoundingClientRect().height;
@@ -125,7 +144,7 @@ function getHeaderHeight(header) {
 }
 
 function scrollHeader(header) {
-    window.addEventListener("scroll", (e) => {
+  window.addEventListener("scroll", (e) => {
     let scrollPos = window.scrollY;
     if (scrollPos >= 10) {
       header.style.padding = "1rem 0";
@@ -137,10 +156,9 @@ function scrollHeader(header) {
   });
 }
 
-function mobileDropdown(button, linksElem){
+function mobileDropdown(button, linksElem) {
   button.addEventListener("click", () => {
     linksElem.classList.toggle("dropdown");
     linksElem.parentNode.classList.toggle("desk");
   });
 }
-
